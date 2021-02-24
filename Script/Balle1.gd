623a48302b2e0444ddae78362c7d4a33d8a03abb
theludovyc <theludovyc@gmail.com>
2021-02-24 22:03:38 +0100

    bounce on wall

--- Script/Balle1.gd
+++ Script/Balle1.gd
@@ -4,7 +4,9 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const GRAV = 200.0
+const SPEED = 200.0
+
+const GRAV = -0.015
 
 var launch := false
 
@@ -19,6 +21,10 @@ onready var linePower = $LinePower
 
 const lineLenght = 200
 
+var vel := Vector2.ZERO
+
+var onGround := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -26,18 +32,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if launch:
-		var dir = (get_viewport().get_mouse_position() - parent.vpSize/2.0).normalized() * lineLenght
+		var dir : Vector2 = (get_viewport().get_mouse_position() - parent.vpSize/2.0).normalized()
 		
-		lineDir.points[1] = dir
+		var dir1 = dir * lineLenght
 		
-		linePower.points[1] = dir * power
+		lineDir.points[1] = dir1
+		
+		dir1 *= power
+		
+		linePower.points[1] = dir1
 		
 		if Input.is_action_pressed("mouse_left"):
 			power = clamp(power + powerInc, 0.0, 1.0)
 		
 		if Input.is_action_just_released("mouse_left"):
-#			launch = false
-			pass
+			launch = false
+			
+			vel = -dir1.normalized() * 1.5
+			
+			lineDir.visible = launch
+			linePower.visible = launch
 		
 	if Input.is_action_just_pressed("ui_accept"):
 		launch = !launch
@@ -49,8 +63,22 @@ func _process(delta):
 #	pass
 
 func _physics_process(delta):
-#	var col = move_and_collide(Vector2(0, GRAV) * delta)
-	pass
+	if !onGround:
+		vel.y -= GRAV
+	elif vel.y > 0.0:
+		vel.y = 0
+		
+	printt(onGround, vel)
+	
+	var col = move_and_collide(vel * SPEED * delta, false)
+	
+	if col != null:
+		if col.normal == Vector2(0, -1):
+			onGround = true
+		else:
+			vel.x = -vel.x
+	else:
+		onGround = false
 
 #func _draw():
 #	draw_line(Vector2.ZERO, dir * power, Color.blue, 4)
